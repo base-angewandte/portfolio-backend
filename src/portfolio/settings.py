9c93502287b9b73a4dd5f1a58dafd4f783f33cc8
Philipp Mayer <philipp.mayer@paradoxon.at>
2018-10-12 11:35:25 +0200

    ~ moved djangoenv

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -23,6 +23,7 @@ from django.urls import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
 
 env = environ.Env()
+env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
