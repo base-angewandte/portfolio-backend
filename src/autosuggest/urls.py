df1d1dec9acde2e503e54a3d791843efbeefad7f
Saranya Balasubramanian <Saranya.Balasubramanian@oeaw.ac.at>
2019-01-15 14:31:09 +0100

    passed the correct parameter

--- src/autosuggest/urls.py
+++ src/autosuggest/urls.py
@@ -3,10 +3,7 @@ from django.conf.urls import include, url
 from rest_framework import routers
 from django.urls import path
 
-#router = routers.SimpleRouter()
-#vrouter.register(r'person', views.PersonViewSet, base_name='person')
-
 urlpatterns = [
-    url('person/', views.lookup_person_view, name='person'),
-    url('place/', views.lookup_place_view, name='place'),
+    path(r'person/<searchstr>/', views.lookup_person_view),
+    path(r'place/<searchstr>/', views.lookup_place_view, name='place'),
 ]
--- src/autosuggest/views.py
+++ src/autosuggest/views.py
@@ -4,27 +4,16 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 
 # Create your views here.
-'''
-class PersonViewSet(viewsets.ViewSet):
-    @api_view(['GET'])
-    def lookup_person_view(request, searchstring, *args, **kwargs):
-        print('ARGS', args, 'KWARGS', kwargs, 'SEARCHSTRING', searchstring)
-        return Response({"id": '1',
-                         "label": 'Garibaldi',
-                         'source': 'GND'})
-'''
-
 @api_view(['GET'])
 def lookup_person_view(request, *args, **kwargs):
-    print('ARGS', args, 'KWARGS', kwargs)
-    print('request', request.GET)
+    searchstr = kwargs.get('searchstr')
     return Response({"id": '1',
-                     "label": 'Garibaldi',
+                     "label": searchstr,
                      'source': 'GND'})
 
 @api_view(['GET'])
 def lookup_place_view(request, *args, **kwargs):
-    print('ARGS', args, 'KWARGS', kwargs)
+    searchstr = kwargs.get('searchstr')
     return Response({"id": '1',
-                     "label": 'Milano',
+                     "label": searchstr,
                      'source': 'GND'})
