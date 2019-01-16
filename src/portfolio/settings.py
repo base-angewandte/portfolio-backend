1c4148ef2eee2be383fb673f7a0b500ba70a89f3
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-01-16 15:48:37 +0100

    + added DEFAULT_RENDERER_CLASSES for DRF

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -332,6 +332,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
     # 'DEFAULT_THROTTLE_CLASSES': (
     #     'rest_framework.throttling.UserRateThrottle'
     # ),
