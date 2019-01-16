0f3f5848ecf7e8a320464d2d1d2e42e9c59f91b1
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-01-16 15:47:59 +0100

    ~ changed TEMPLATES setting

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -150,7 +150,7 @@ ROOT_URLCONF = '{}.urls'.format(PROJECT_NAME)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
