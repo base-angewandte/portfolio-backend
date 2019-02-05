14f70ef4b5f40703e884b752893aeda395d8287f
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-02-05 13:33:25 +0100

    + added superusers

--- src/general/apps.py
+++ src/general/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class GeneralConfig(AppConfig):
     name = 'general'
+
+    def ready(self):
+        # import signal handlers
+        from . import signals
--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -73,6 +73,10 @@ ADMINS = getaddresses([env('DJANGO_ADMINS', default='Philipp Mayer <philipp.maye
 
 MANAGERS = ADMINS
 
+SUPERUSERS = env.tuple('SUPERUSERS', default=(
+    '***REMOVED***',  # Philipp Mayer
+))
+
 # Application definition
 
 INSTALLED_APPS = [
