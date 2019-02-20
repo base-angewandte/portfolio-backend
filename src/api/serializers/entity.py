8db28163a9c74aa3739e4602fe4b155eb0e9ccf4
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-02-20 12:34:21 +0100

    + added additional informations to relations in EntitySerializer

--- src/api/serializers/entity.py
+++ src/api/serializers/entity.py
@@ -1,8 +1,9 @@
 from collections import OrderedDict
 
+from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
-from core.models import Entity
+from core.models import Entity, Relation
 from media_server.models import get_image_for_entity
 from . import CleanModelSerializer
 from .fields import SwaggerSerializerField
@@ -14,9 +15,15 @@ class RelatedEntitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Entity
         fields = ('id', 'title', 'type', 'image', )
+        read_only_fields = fields
 
     def get_image(self, obj) -> str:
         return get_image_for_entity(obj.pk)
+
+
+class RelationsSerializer(serializers.Serializer):
+    id = serializers.CharField(read_only=True)
+    to = RelatedEntitySerializer(read_only=True)
 
 
 class EntityModelSerializer(serializers.ModelSerializer):
@@ -55,8 +62,23 @@ class EntitySerializer(CleanModelSerializer):
         attrs=OrderedDict([('hidden', True)]),
         **ems.fields.get('published')._kwargs,
     )
-    relations = RelatedEntitySerializer(many=True, read_only=True)
+    relations = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Entity
         fields = '__all__'
+
+    @swagger_serializer_method(serializer_or_field=RelationsSerializer)
+    def get_relations(self, obj):
+        ret = []
+        for relation in Relation.objects.select_related('to_entity').filter(from_entity=obj):
+            ret.append({
+                'id': relation.pk,
+                'to': {
+                    'id': relation.to_entity.pk,
+                    'title': relation.to_entity.title,
+                    'type': relation.to_entity.type,
+                    'image': get_image_for_entity(relation.to_entity.pk),
+                }
+            })
+        return ret
