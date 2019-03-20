46c034f22d5f2996e11d08cbeaf70064658d1e89
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-03-20 13:54:31 +0100

    ~ restrict fieldname to possible choices

--- src/autosuggest/urls.py
+++ src/autosuggest/urls.py
@@ -1,9 +1,18 @@
+from django.conf import settings
 from django.conf.urls import url
 
 from . import views
 
 # NOTE: fieldname must be present in ACTIVE_SOURCES in portfolio/settings for this to work
 urlpatterns = [
-    url(r'^(?P<fieldname>(\w+))/$', views.lookup_view, name='lookup_all'),
-    url(r'^(?P<fieldname>(\w+))/(?P<searchstr>(.+))/$', views.lookup_view_search, name='lookup'),
+    url(
+        r'^(?P<fieldname>({}))/$'.format('|'.join(settings.ACTIVE_SOURCES.keys())),
+        views.lookup_view,
+        name='lookup_all',
+    ),
+    url(
+        r'^(?P<fieldname>({}))/(?P<searchstr>(.+))/$'.format('|'.join(settings.ACTIVE_SOURCES.keys())),
+        views.lookup_view_search,
+        name='lookup',
+    ),
 ]
