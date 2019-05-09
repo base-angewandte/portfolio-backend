d0abab7d5932772f527db22570e31043a592bb8e
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-05-09 14:32:15 +0200

    - removed TODO

--- src/core/schemas/award.py
+++ src/core/schemas/award.py
@@ -17,7 +17,7 @@ class AwardSchema(Schema):
     category = fields.Str(**{'x-attrs': {'order': 1}})
     winner = get_contributors_field_for_role('winner', {'order': 2})
     granted_by = get_contributors_field_for_role('granted_by', {'order': 3})
-    jury = get_contributors_field_for_role('jury', {'order': 4})  # TODO: not sortable according to objects and forms
+    jury = get_contributors_field_for_role('jury', {'order': 4})
     contributors = get_contributors_field({'order': 5})
     date_location = get_date_location_group_field({'order': 6})
     award_ceremony = get_date_time_field({'field_format': 'half', 'order': 7})
