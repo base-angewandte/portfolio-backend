3552c6a69d78dac5f684ee9368a95207d62eb3cc
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-06-05 16:01:18 +0200

    + check if license is in get_media_licenses

--- src/media_server/validators.py
+++ src/media_server/validators.py
@@ -1,7 +1,10 @@
+import json
+
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from jsonschema import validate, ValidationError as SchemaValidationError
 
+from core.skosmos import get_media_licenses
 from .schemas import get_license_jsonschema
 
 
@@ -11,3 +14,7 @@ def validate_license(value):
     except SchemaValidationError as e:
         msg = _('Invalid license: %(error)s') % {'error': e.message}
         raise ValidationError(msg)
+
+    license_set = set(json.dumps(d, sort_keys=True) for d in get_media_licenses())
+    if json.dumps(value, sort_keys=True) not in license_set:
+        raise ValidationError(_('Invalid license'))
