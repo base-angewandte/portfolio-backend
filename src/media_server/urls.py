e55c8c27225b3bdb48c9b98b2ad2668f4daf46f2
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-06-06 16:03:26 +0200

    ~ changed url to re_path

--- src/media_server/urls.py
+++ src/media_server/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.urls import path
+from django.urls import re_path
 
 from .views import protected_view
 
 urlpatterns = [
-    path('<path:path>', protected_view, {'server': settings.PROTECTED_MEDIA_SERVER, }, name='protected_media'),
+    re_path(r'^(?P<path>.*)$', protected_view, {'server': settings.PROTECTED_MEDIA_SERVER, }, name='protected_media'),
 ]
