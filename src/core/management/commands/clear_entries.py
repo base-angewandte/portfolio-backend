fc4b472ca44c31ff0645b1390ba542f0487363de
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-07-24 11:53:29 +0200

    + added progressbar to clear_entries

--- src/core/management/commands/clear_entries.py
+++ src/core/management/commands/clear_entries.py
@@ -1,3 +1,5 @@
+from progressbar import progressbar
+
 from django.core.management.base import BaseCommand
 
 from core.models import Entry
@@ -10,7 +12,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         b = boolean_input('Are you really sure you want to delete all entries? [y/N] ')
         if b:
-            Entry.objects.all().delete()
+            for e in progressbar(Entry.objects.all()):
+                e.delete()
             self.stdout.write(self.style.SUCCESS('Successfully deleted all entries'))
         else:
             self.stdout.write('Aborted')
