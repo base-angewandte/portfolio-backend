953b285b754dabb9ceb6082f77655bade2237e83
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-11-14 12:36:03 +0100

    + added max_requests_jitter

--- src/gunicorn-conf.py
+++ src/gunicorn-conf.py
@@ -5,6 +5,7 @@ bind = ":{}".format(os.getenv("GUNICORN_PORT", "8200"))
 
 keepalive = 120
 max_requests = 1000
+max_requests_jitter = 50
 timeout = 300
 workers = os.getenv("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1)
 worker_tmp_dir = "/dev/shm"
