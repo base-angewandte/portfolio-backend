65a858f61a0b3a99af2d000a05fcbfd94c67199c
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-11-14 11:44:09 +0100

    + make gunicorn port and workers configurable

--- src/gunicorn-conf.py
+++ src/gunicorn-conf.py
@@ -1,11 +1,12 @@
 import multiprocessing
+import os
 
-bind = ":8200"
+bind = ":{}".format(os.getenv("GUNICORN_PORT", "8200"))
 
 keepalive = 120
 max_requests = 1000
 timeout = 300
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = os.getenv("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1)
 worker_tmp_dir = "/dev/shm"
 
 loglevel = "info"
