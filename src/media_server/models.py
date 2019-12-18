77ed61f87c8abfcd5f62aaf2e72333c7d49630c5
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-12-18 13:22:26 +0100

    ~ bugfix exif fallback data

--- src/media_server/models.py
+++ src/media_server/models.py
@@ -291,14 +291,12 @@ class Media(models.Model):
         except Exception:
             logger.warning('Could not read metainformation from file: %s', self.file.path)
             # create fallback data
-            self.exif = json.dumps(
-                [
-                    {
-                        'FileSize': {'desc': 'File Size', 'num': self.file.size, 'val': humanize_size(self.file.size)},
-                        'MIMEType': {'desc': 'MIME Type', 'val': self.mime_type},
-                    }
-                ]
-            )
+            self.exif = [
+                {
+                    'FileSize': {'desc': 'File Size', 'num': self.file.size, 'val': humanize_size(self.file.size)},
+                    'MIMEType': {'desc': 'MIME Type', 'val': self.mime_type},
+                }
+            ]
         self.save()
 
 
