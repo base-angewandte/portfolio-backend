44366b1002fe6b286ecbfa2f7793571184c6d379
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2020-06-29 15:51:21 +0200

    fix: title missing from research project schema for funding category

--- src/core/schemas/entries/research_project.py
+++ src/core/schemas/entries/research_project.py
@@ -1,9 +1,9 @@
 from marshmallow import fields
 
 from ...schemas import ICON_EVENT
-from ...skosmos import get_collection_members
+from ...skosmos import get_collection_members, get_preflabel_lazy
 from ..base import BaseSchema
-from ..general import get_contributors_field, get_contributors_field_for_role, get_date_range_field, get_url_field
+from ..general import get_contributors_field, get_contributors_field_for_role, get_date_range_field, get_url_field, get_string_field
 from ..utils import years_from_date_range
 
 ICON = ICON_EVENT
@@ -17,7 +17,8 @@ class ResearchProjectSchema(BaseSchema):
     project_lead = get_contributors_field_for_role('project_lead', {'order': 1})
     project_partnership = get_contributors_field_for_role('project_partnership', {'order': 2})
     funding = get_contributors_field_for_role('funding', {'order': 3})
-    funding_category = fields.Str(**{'x-attrs': {'order': 4}})
+    funding_category = get_string_field(
+        get_preflabel_lazy('funding_category'), {'order': 4})
     contributors = get_contributors_field({'order': 5})
     date_range = get_date_range_field({'order': 6, 'field_format': 'full'})
     url = get_url_field({'order': 7})
