f9106b90a9156150c37565f87bb5839829ef65bf
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2021-05-11 08:49:39 +0200

    + add code to push untransformed entries to showroom

--- src/core/management/commands/push_to_showroom.py
+++ src/core/management/commands/push_to_showroom.py
@@ -1,5 +1,7 @@
 from re import match
 
+import requests
+
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
@@ -36,6 +38,53 @@ class Command(BaseCommand):
         else:
             entries = Entry.objects.filter(pk__in=options['id'])
 
-        print(entries)
-        self.stdout.write(self.style.SUCCESS('Successfully pushed entries:'))
-        # TODO: output number of new and updated entries
+        created = []
+        updated = []  # TODO: do we want to differentiate between updated and newly pushed entries?
+        not_pushed = []
+        for entry in entries:
+            data = {
+                'title': entry.title,
+                'belongs_to': str(entry.owner),
+                'source_repo': settings.SHOWROOM_REPO_ID,
+                'type': entry.type,
+                'list': '[]',
+                'primary_details': '[]',
+                'secondary_details': entry.data,
+                'locations': '[]',
+                'dates': '[]',
+            }
+
+            # TODO: testing code
+            if settings.DEBUG:
+                # TODO: we need this for dev until user profile is syncing entities
+                data['belongs_to'] = 'mYDG78x7RuPGBHSZgfFLtC'
+                # if entry.id in ['8gtpjPKkD2xcBqvEoestpn', 'oHW4dqHEhApPvLEUEMrFun']:
+                #    data['belongs_to'] = 'ladida'
+
+            headers = {
+                'X-Api-Client': str(settings.SHOWROOM_REPO_ID),
+                'X-Api-Key': settings.SHOWROOM_API_KEY,
+            }
+            r = requests.post(settings.SHOWROOM_API_BASE + 'activities/', json=data, headers=headers)
+
+            if r.status_code == 403:
+                raise CommandError(f'Authentication failed: {r.text}')
+
+            elif r.status_code == 400:
+                not_pushed.append(entry.id)
+                self.stdout.write(self.style.WARNING(f'Entry {entry.id} could not be pushed: 400: {r.text}'))
+
+            # according to the api spec the only other option is a 201 response when the entry was pushed
+            # but we should also check for anything unexpected to happen
+            elif r.status_code == 201:
+                # so far Showroom does not differentiate between updated and created entries, so we'll add it to created
+                created.append(entry.id)
+            else:
+                raise CommandError(f'Ouch! Something unexpected happened: {r.status_code} {r.text}')
+
+        self.stdout.write(self.style.SUCCESS(f'Successfully pushed {len(created)+len(updated)} entries:'))
+        self.stdout.write(f'Created: {len(created)}')
+        self.stdout.write(f'Updated: {len(updated)}')
+        if len(not_pushed) > 0:
+            self.stdout.write(self.style.WARNING(f'Could not push {len(not_pushed)} entries:'))
+            self.stdout.write(str(not_pushed))
