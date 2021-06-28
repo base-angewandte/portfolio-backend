604bb30be78ba8c4315cc982c3db45a8c88151d4
b0021902 <philip.roeggla@oeaw.ac.at>
2021-06-28 12:24:02 +0200

    External Server Error Exception

---
+++ src/media_server/archiver/interface/exceptions.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod
+
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
+
+class ExternalServerError(APIException):
+    """External has an error."""
+
+    status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+
+    @abstractmethod
+    @property
+    def external_service_name(self) -> str:
+        """
+        :return: The name of the external service
+        """
+        pass
+
+    @property
+    def default_detail(self) -> str:
+        return f'{self.external_service_name} temporarily unavailable, try again later.'
+
+    @property
+    def default_code(self) -> str:
+        return 'service_unavailable'
