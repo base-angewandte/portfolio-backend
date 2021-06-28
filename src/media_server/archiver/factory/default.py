4ffa29ad4d81cf50bd86d971f7e8bfece7da2b6d
b0021902 <philip.roeggla@oeaw.ac.at>
2021-06-28 12:19:02 +0200

    The archiver factory

---
+++ src/media_server/archiver/factory/default.py
@@ -0,0 +1,40 @@
+import typing
+from typing import TYPE_CHECKING, Dict
+
+import environ
+
+from ..implementations.phaidra.main import PhaidraArchiver
+from .archives import Archives
+
+if TYPE_CHECKING:
+    from ..interface.abstractarchiver import AbstractArchiver
+    from ..interface.archiveobject import ArchiveObject
+
+_environment = environ.Env()
+_environment.read_env()
+
+
+class ArchiverFactory:
+    """Generate instances of implementations of `AbstractArchiver`s."""
+
+    mappings: Dict[Archives, typing.Type['AbstractArchiver']] = {Archives.PHAIDRA: PhaidraArchiver}
+
+    def create(
+        self, archive_object: 'ArchiveObject', archive: typing.Optional[typing.Union[Archives, str]] = None
+    ) -> 'AbstractArchiver':
+        """Create a archiver instance for an entry.
+
+        :param archive_object:
+        :param archive: defaults to env ARCHIVE_TYPE
+        :return: An implementation for an archiver
+        """
+        archive = archive or _environment.str('ARCHIVE_TYPE')
+        if archive.__class__ is str:
+            try:
+                archive = Archives(archive)
+            except ValueError:
+                raise NotImplementedError(f'{archive} is not registered in the factory')
+        if archive not in self.mappings:
+            raise NotImplementedError(f'{archive} is not registered in the factory')
+
+        return self.mappings[archive](archive_object)
