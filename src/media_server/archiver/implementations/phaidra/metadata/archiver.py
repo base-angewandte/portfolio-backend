e8e7740f149536040466618f30140b31aba2f170
b0021902 <philip.roeggla@oeaw.ac.at>
2021-07-05 17:16:48 +0200

    Archiver controller

--- src/media_server/archiver/implementations/phaidra/metadata/archiver.py
+++ src/media_server/archiver/implementations/phaidra/metadata/archiver.py
@@ -1,8 +1,31 @@
+from typing import TYPE_CHECKING, Dict, Optional
+
+from .datatranslation import PhaidraMetaDataTranslator
+from .mappings.contributormapping import BidirectionalConceptsMapper
+from .schemas import get_phaidra_meta_data_schema_with_dynamic_fields
+
+if TYPE_CHECKING:
+    from ....interface.archiveobject import ArchiveObject
 from ....interface.abstractarchiver import AbstractArchiver
 
 
 class DefaultMetadataArchiver(AbstractArchiver):
-    pass
+
+    data: Optional[Dict] = None
+
+    def __init__(self, archive_object: 'ArchiveObject'):
+        super().__init__(archive_object)
+        self.data = None
+
+    def validate(self) -> None:
+        contributor_role_mapping = BidirectionalConceptsMapper.from_entry(self.archive_object.entry)
+        translator = PhaidraMetaDataTranslator()
+        data = translator.translate_data(self.archive_object.entry, contributor_role_mapping)
+        schema = get_phaidra_meta_data_schema_with_dynamic_fields(contributor_role_mapping)
+        result = schema.load(data)
+        errors: dict = result.errors
+        self.data = result.data
+        self.throw_validation_errors(translator.translate_errors(errors))
 
 
 class ThesisMetadataArchiver(AbstractArchiver):
