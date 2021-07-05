3edd3ef5ccb871de1b308e3208ae51515a96d732
b0021902 <philip.roeggla@oeaw.ac.at>
2021-07-05 11:19:00 +0200

    Only validate on not validated, not on empty data

--- src/media_server/archiver/implementations/phaidra/metadata/datatranslation.py
+++ src/media_server/archiver/implementations/phaidra/metadata/datatranslation.py
@@ -9,32 +9,34 @@ from media_server.archiver.implementations.phaidra.abstracts.datatranslation imp
 class PhaidraMetaDataTranslator(AbstractDataTranslator):
     def translate_data(self, model: 'Entry') -> dict:
         return {
-            'dcterms:type': self.get_dcterms_type(),
-            'edm:hasType': self.get_edm_hasType(model),
+            'dcterms:type': self._get_dcterms_type(),
+            'edm:hasType': self._get_edm_hasType(model),
             'dce:title': self.get_dc_title(model),
-            'dcterms:subject': self.get_dcterms_subject(model),
-            'rdau:P60048': self.get_type_match_label_list(
+            'dcterms:subject': self._get_dcterms_subject(model),
+            'rdau:P60048': self._get_type_match_label_list(
                 model,
                 [
                     'material',
                 ],
             ),
-            'dce:format': self.get_type_match_label_list(
+            'dce:format': self._get_type_match_label_list(
                 model,
                 [
                     'format',
                 ],
             ),
-            'bf:note': self.get_get_bf_note(model),
-            'role:edt': self.get_role_by(model, 'editors', 'http://base.uni-ak.ac.at/portfolio/vocabulary/editor'),
-            'role:aut': self.get_role_by(model, 'authors', 'http://base.uni-ak.ac.at/portfolio/vocabulary/author'),
-            'role:pbl': self.get_role_by(model, 'role:pbl', 'http://base.uni-ak.ac.at/portfolio/vocabulary/publisher'),
+            'bf:note': self._get_get_bf_note(model),
+            'role:edt': self._get_role_by(model, 'editors', 'http://base.uni-ak.ac.at/portfolio/vocabulary/editor'),
+            'role:aut': self._get_role_by(model, 'authors', 'http://base.uni-ak.ac.at/portfolio/vocabulary/author'),
+            'role:pbl': self._get_role_by(
+                model, 'role:pbl', 'http://base.uni-ak.ac.at/portfolio/vocabulary/publisher'
+            ),
         }
 
     def translate_errors(self, errors: Optional[Dict]) -> Dict:
         raise NotImplementedError()
 
-    def get_dcterms_type(self) -> List:
+    def _get_dcterms_type(self) -> List:
         return [
             {
                 '@type': 'skos:Concept',
@@ -43,23 +45,23 @@ class PhaidraMetaDataTranslator(AbstractDataTranslator):
             }
         ]
 
-    def get_edm_hasType(self, model: 'Entry') -> List:
+    def _get_edm_hasType(self, model: 'Entry') -> List:
         return [
             {
                 '@type': 'skos:Concept',
-                'skos:prefLabel': self.get_skos_prefLabel_from_entry(model),
-                'skos:exactMatch': self.get_skos_exactMatch(model),
+                'skos:prefLabel': self._get_skos_prefLabel_from_entry(model),
+                'skos:exactMatch': self._get_skos_exactMatch(model),
             }
         ]
 
-    def get_skos_prefLabel_from_entry(self, model: 'Entry') -> List:
+    def _get_skos_prefLabel_from_entry(self, model: 'Entry') -> List:
         try:
             items: Dict = model.type['label']['items']
         except KeyError:
             return []
         return [{'@value': label, '@language': language} for language, label in items.items()]
 
-    def get_skos_exactMatch(self, model: 'Entry') -> List:
+    def _get_skos_exactMatch(self, model: 'Entry') -> List:
         try:
             return [
                 model.type['source'],
@@ -82,17 +84,17 @@ class PhaidraMetaDataTranslator(AbstractDataTranslator):
             }
         ]
 
-    def get_dcterms_subject(self, model: 'Entry') -> List:
+    def _get_dcterms_subject(self, model: 'Entry') -> List:
         return [
             {
                 '@type': 'skos:Concept',
                 'skos:exactMatch': [keyword['source']] if 'source' in keyword else [],
-                'skos:prefLabel': self.get_skos_prefLabel_from_label_items_dict(keyword),
+                'skos:prefLabel': self._get_skos_prefLabel_from_label_items_dict(keyword),
             }
             for keyword in model.keywords
         ]
 
-    def get_skos_prefLabel_from_label_items_dict(self, container: Dict) -> List:
+    def _get_skos_prefLabel_from_label_items_dict(self, container: Dict) -> List:
         try:
             labels: Dict = container['label']['items']
             return [
@@ -105,7 +107,7 @@ class PhaidraMetaDataTranslator(AbstractDataTranslator):
         except KeyError:
             return []
 
-    def get_type_match_label_list(self, model: 'Entry', data_keys: List[str]) -> List:
+    def _get_type_match_label_list(self, model: 'Entry', data_keys: List[str]) -> List:
         try:
             items = model.data
             for data_key in data_keys:
@@ -117,12 +119,12 @@ class PhaidraMetaDataTranslator(AbstractDataTranslator):
             {
                 '@type': 'skos:Concept',
                 'skos:exactMatch': [item['source']] if 'source' in item else [],
-                'skos:prefLabel': self.get_skos_prefLabel_from_label_items_dict(item),
+                'skos:prefLabel': self._get_skos_prefLabel_from_label_items_dict(item),
             }
             for item in items
         ]
 
-    def get_get_bf_note(self, model: 'Entry') -> List:
+    def _get_get_bf_note(self, model: 'Entry') -> List:
         abstract_source = 'http://base.uni-ak.ac.at/portfolio/vocabulary/abstract'
         text: Dict
         texts = [
@@ -133,12 +135,12 @@ class PhaidraMetaDataTranslator(AbstractDataTranslator):
         return [
             {
                 '@type': 'bf:Summary' if 'type' in text else 'bf:Note',
-                'skos:prefLabel': self.get_skos_prefLabel_from_text(text),
+                'skos:prefLabel': self._get_skos_prefLabel_from_text(text),
             }
             for text in texts
         ]
 
-    def get_skos_prefLabel_from_text(self, text: Dict) -> List:
+    def _get_skos_prefLabel_from_text(self, text: Dict) -> List:
         try:
             text_data = text['data']
         except KeyError:
@@ -149,7 +151,7 @@ class PhaidraMetaDataTranslator(AbstractDataTranslator):
             if ('text' in text_datum) and ('language' in text_datum) and ('source' in text_datum['language'])
         ]
 
-    def get_role_by(self, model: 'Entry', main_level: str, role_uri: str) -> List:
+    def _get_role_by(self, model: 'Entry', main_level: str, role_uri: str) -> List:
         has_main_level_persons = main_level in model.data
         has_contributors = 'contributors' in model.data
         if not has_main_level_persons and not has_contributors:
