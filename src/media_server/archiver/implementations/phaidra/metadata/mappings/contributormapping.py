cbb5fc66bfd6302490c547394a2f082eeca80c6a
b0021902 <philip.roeggla@oeaw.ac.at>
2021-07-05 16:47:08 +0200

    Trying to avoid git hook pyupgrade feature by not using annotations

--- src/media_server/archiver/implementations/phaidra/metadata/mappings/contributormapping.py
+++ src/media_server/archiver/implementations/phaidra/metadata/mappings/contributormapping.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import typing
 from dataclasses import dataclass, field
 
@@ -17,10 +15,10 @@ class ConceptMapper:
     uri: str
 
     '''comparables, eg  {'http://vocab.getty.edu/aat/300160216', 'http://d-nb.info/gnd/4005565-6'}'''
-    owl_sameAs: set[str]
+    owl_sameAs: typing.Set[str]
 
     @classmethod
-    def from_base_uri(cls, uri: str) -> ConceptMapper:
+    def from_base_uri(cls, uri: str) -> 'ConceptMapper':
         graph = get_json_data(uri)['graph']
         for node in graph:
             if node['uri'] == uri:
@@ -39,27 +37,27 @@ class ConceptMapper:
 
 @dataclass
 class BidirectionalConceptsMapper:
-    concept_mappings: dict[str, ConceptMapper] = field(default_factory=dict)
+    concept_mappings: typing.Dict[str, ConceptMapper] = field(default_factory=dict)
 
     @classmethod
-    def from_base_uris(cls, uris: set[str]) -> BidirectionalConceptsMapper:
+    def from_base_uris(cls, uris: typing.Set[str]) -> 'BidirectionalConceptsMapper':
         return cls(concept_mappings={uri: ConceptMapper.from_base_uri(uri) for uri in uris})
 
-    def get_owl_sameAs_from_uri(self, uri: str) -> set[str]:
+    def get_owl_sameAs_from_uri(self, uri: str) -> typing.Set[str]:
         return self.concept_mappings[uri].owl_sameAs
 
-    def get_uris_from_owl_sameAs(self, owl_sameAs: str) -> set[str]:
+    def get_uris_from_owl_sameAs(self, owl_sameAs: str) -> typing.Set[str]:
         return {
             uri for uri, concept_mapper in self.concept_mappings.items() if owl_sameAs in concept_mapper.owl_sameAs
         }
 
-    def add_uri(self, uri: str) -> BidirectionalConceptsMapper:
+    def add_uri(self, uri: str) -> 'BidirectionalConceptsMapper':
         if uri not in self.concept_mappings:
             self.concept_mappings[uri] = ConceptMapper.from_base_uri(uri)
         return self
 
     @classmethod
-    def from_entry(cls, entry: 'Entry') -> BidirectionalConceptsMapper:
+    def from_entry(cls, entry: 'Entry') -> 'BidirectionalConceptsMapper':
         if 'contributors' not in entry.data:
             return cls.from_base_uris(set())
         contributors = entry.data['contributors']
