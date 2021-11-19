6694b600f0139100b9adc920329da419848c746f
b0021902 <philip.roeggla@oeaw.ac.ar>
2021-11-19 16:05:08 +0100

    * Replaced client patch use with orm, since it was buggy * Licenses as constants

--- src/media_server/archiver/implementations/phaidra/phaidra_tests/test_full_consumer_logic/test_media_update.py
+++ src/media_server/archiver/implementations/phaidra/phaidra_tests/test_full_consumer_logic/test_media_update.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
@@ -24,6 +26,11 @@ class OnePdfResaved(APITestCase):
         client = self.client
         client.login(username='Hansi', password='Hansi1986')
 
+        # define license_states for testing
+        self.first_portfolio_license = 'http://base.uni-ak.ac.at/portfolio/licenses/copyright'
+        self.second_portfolio_license = 'http://base.uni-ak.ac.at/portfolio/licenses/CC-BY-NC-4.0'
+        self.second_phaidra_license = 'http://creativecommons.org/licenses/by-nc/4.0/'
+
         # Create an entry …
         response = client.post('/api/v1/entry/', data={'title': 'Test'})
         entry_id = response.data['id']
@@ -35,7 +42,10 @@ class OnePdfResaved(APITestCase):
                 'file': SimpleUploadedFile('example.pdf', b'example file'),
                 'entry': entry_id,
                 'published': 'false',
-                'license': '{"source":"http://base.uni-ak.ac.at/portfolio/licenses/copyright","label":{"de":"urheberrechtlich geschützt","en":"Copyright"}}',
+                'license': json.dumps({
+                    'source': self.first_portfolio_license,
+                    'label': {'de': 'urheberrechtlich geschützt', "en": "Copyright"}
+                }),
             },
         )
         media_id = response.data
@@ -46,22 +56,27 @@ class OnePdfResaved(APITestCase):
         response = client.get(
             f'/api/v1/archive_assets/media/{media_id}/',
         )
+        if response.status_code != 200:
+            raise RuntimeError(f'testserver responded with code {response.status_code} and content {response.json()}')
+
         archive_id = response.data['object']
         worker = django_rq.get_worker('high')
         worker.work(burst=True)  # wait until it is done
 
         # change media
-        response = client.patch(
-            f'/api/v1/media/{media_id}/',
-            data={
-                'license':  '{"label":{"en":"Creative Commons Attribution Non-Commercial 4.0"},"source":"http://base.uni-ak.ac.at/portfolio/licenses/CC-BY-NC-4.0"}',
-            },
-        )
+        media = Media.objects.get(pk=media_id)
+        media.license = {
+                            'label': {'en': "Creative Commons Attribution Non-Commercial 4.0"},
+                            "source": self.second_portfolio_license,
+                        }
+        media.save()
 
         # update archive
         response = client.put(
             f'/api/v1/archive?entry={entry_id}',
         )
+        if response.status_code != 200:
+            raise RuntimeError(f'testserver responded with code {response.status_code} and content {response.json()}')
 
         # byby ;-)
         client.logout()
@@ -73,9 +88,8 @@ class OnePdfResaved(APITestCase):
     def test_update_reflected_in_phaidra(self):
         media = Media.objects.get(pk=self.media_id)
         response = requests.get(f'https://services.phaidra-sandbox.univie.ac.at/api/object/{media.archive_id}/jsonld')
-        licences = response.json()['edm:rights']
+        phaidra_licenses = response.json()['edm:rights']
         self.assertEqual(
-            licences,
-            ['http://creativecommons.org/licenses/by-nc/4.0/']
+            phaidra_licenses,
+            [self.second_phaidra_license, ]
         )
-
