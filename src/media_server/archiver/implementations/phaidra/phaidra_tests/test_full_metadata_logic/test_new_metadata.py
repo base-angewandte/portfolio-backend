888c2efc90ad8834398b27736f0e07b03bf7ac6b
b0021902 <philip.roeggla@oeaw.ac.ar>
2021-12-16 13:45:24 +0100

    Failing Tests for Feature #1677

---
+++ src/media_server/archiver/implementations/phaidra/phaidra_tests/test_full_metadata_logic/test_new_metadata.py
@@ -0,0 +1,117 @@
+"""
+New metadata is added to the containers. Check it
+"""
+
+from django.test import TestCase
+
+from media_server.archiver.implementations.phaidra.metadata.default.schemas import PhaidraMetaData
+from media_server.archiver.implementations.phaidra.metadata.default.datatranslation import PhaidraMetaDataTranslator
+from media_server.archiver.implementations.phaidra.metadata.mappings.contributormapping import \
+    BidirectionalConceptsMapper
+from media_server.archiver.implementations.phaidra.phaidra_tests.utillities import ModelProvider
+
+
+class TestFeature1677(TestCase):
+    # Test this data
+    translated_data_with_one_location: dict
+    translated_data_with_no_location: dict
+    translated_data_with_two_locations: dict
+    validation_with_one_location: dict
+    validation_with_no_location: dict
+    validation_with_two_locations: dict
+
+    # Use this data to test
+    location_label_1 = 'Universität für Angewandte Kunst Wien'
+    location_label_2 = 'Wien, Österreich'
+
+    location_1_portfolio = {'country': 'Austria',
+                            'geometry': {'coordinates': [16.348388, 48.198674], 'type': 'Point'},
+                            'label': location_label_1,
+                            'locality': 'Vienna',
+                            'region': 'Wien',
+                            'source': 'https://api.geocode.earth/v1/place?ids=whosonfirst:locality:101748073'
+                            }
+
+    location_2_portfolio = {'country': 'Österreich',
+                            'geometry': {'coordinates': [16.382464, 48.208126], 'type': 'Point'},
+                            'house_number': '2',
+                            'label': location_label_2,
+                            'locality': 'Wien',
+                            'postcode': '1010',
+                            'region': 'Wien',
+                            'source': 'https://api.geocode.earth/v1/place?ids=openstreetmap:venue:way/25427674',
+                            'street': 'Oskar-Kokoschka-Platz'}
+
+    # Expected data
+
+    location_1_phaidra = {
+        '@value': location_label_1,
+        '@language': 'und',
+    }
+
+    location_2_phaidra = {
+        '@value': location_label_2,
+        '@language': 'und',
+    }
+
+    @classmethod
+    def setUpTestData(cls):
+        """
+        1) Set up data and validation with location
+        2) Set up data and validation without location
+        2) Set up data and validation with two locations
+        :return:
+        """
+        model_provider = ModelProvider()
+
+        entry_with_no_location = model_provider.get_entry(thesis_type=False)
+
+        entry_with_one_location = model_provider.get_entry(thesis_type=False)
+        entry_with_one_location.data['locations'] = [cls.location_1_portfolio, ]
+        entry_with_one_location.save()
+        entry_with_one_location.refresh_from_db()
+
+        entry_with_two_locations = model_provider.get_entry(thesis_type=False)
+        entry_with_two_locations.data['locations'] = [cls.location_1_portfolio, cls.location_2_portfolio]
+        entry_with_two_locations.save()
+        entry_with_two_locations.refresh_from_db()
+
+        mapping_with_no_location = BidirectionalConceptsMapper.from_entry(entry_with_no_location)
+        mapping_with_one_location = BidirectionalConceptsMapper.from_entry(entry_with_one_location)
+        mapping_with_two_locations = BidirectionalConceptsMapper.from_entry(entry_with_two_locations)
+
+        translator_0_locations = PhaidraMetaDataTranslator(mapping_with_no_location)
+        translator_1_location = PhaidraMetaDataTranslator(mapping_with_one_location)
+        translator_2_locations = PhaidraMetaDataTranslator(mapping_with_two_locations)
+
+        schema_0_locations = PhaidraMetaData()
+        schema_1_location = PhaidraMetaData()
+        schema_2_locations = PhaidraMetaData()
+
+        cls.translated_data_with_no_location = translator_0_locations.translate_data(entry_with_no_location)
+        cls.translated_data_with_one_location = translator_1_location.translate_data(entry_with_one_location)
+        cls.translated_data_with_two_locations = translator_2_locations.translate_data(entry_with_two_locations)
+
+        cls.validation_with_no_location = schema_0_locations.validate(cls.translated_data_with_no_location)
+        cls.validation_with_one_location = schema_1_location.validate(cls.translated_data_with_one_location)
+        cls.validation_with_two_locations = schema_2_locations.validate(cls.translated_data_with_two_locations)
+
+    def test_translated_data(self):
+        self.assertIn('bf:physicalLocation', self.translated_data_with_no_location['metadata']['json-ld'])
+        self.assertIn('bf:physicalLocation', self.translated_data_with_one_location['metadata']['json-ld'])
+        self.assertIn('bf:physicalLocation', self.translated_data_with_two_locations['metadata']['json-ld'])
+
+        self.assertEqual([], self.translated_data_with_no_location['metadata']['json-ld']['bf:physicalLocation'])
+        self.assertEqual(
+            [self.location_1_phaidra, ],
+            self.translated_data_with_one_location['metadata']['json-ld']['bf:physicalLocation']
+        )
+        self.assertEqual(
+            [self.location_1_phaidra, self.location_2_phaidra],
+            self.translated_data_with_two_locations['metadata']['json-ld']['bf:physicalLocation']
+        )
+
+    def test_validation(self):
+        self.assertEqual({}, self.validation_with_no_location)
+        self.assertEqual({}, self.validation_with_two_locations)
+        self.assertEqual({}, self.validation_with_two_locations)
