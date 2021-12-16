3b3653effd3143065e63c44bae0b6c6f33a457f6
b0021902 <philip.roeggla@oeaw.ac.ar>
2021-12-16 15:16:05 +0100

    Full working phaidra test for Feature #1677

--- src/media_server/archiver/implementations/phaidra/phaidra_tests/test_full_metadata_logic/test_new_metadata.py
+++ src/media_server/archiver/implementations/phaidra/phaidra_tests/test_full_metadata_logic/test_new_metadata.py
@@ -1,14 +1,16 @@
 """
 New metadata is added to the containers. Check it
 """
-
+import IPython
+import requests as requests
 from django.test import TestCase
 
 from media_server.archiver.implementations.phaidra.metadata.default.schemas import PhaidraMetaData, \
     ValueLanguageBaseSchema
 from media_server.archiver.implementations.phaidra.metadata.thesis.schemas import create_dynamic_phaidra_thesis_meta_data_schema
 from media_server.archiver.implementations.phaidra.metadata.default.datatranslation import PhaidraMetaDataTranslator
-from media_server.archiver.implementations.phaidra.phaidra_tests.utillities import FakeBidirectionalConceptsMapper
+from media_server.archiver.implementations.phaidra.phaidra_tests.utillities import FakeBidirectionalConceptsMapper, \
+    ClientProvider
 from media_server.archiver.implementations.phaidra.phaidra_tests.utillities import ModelProvider
 from media_server.archiver.implementations.phaidra.utillities.fields import PortfolioNestedField
 
@@ -21,6 +23,8 @@ class TestFeature1677(TestCase):
     validation_with_one_location: dict
     validation_with_no_location: dict
     validation_with_two_locations: dict
+    phaidra_data_with_1_location: dict
+    phaidra_data_with_2_locations: dict
 
     # Use this data to test
     location_label_1 = 'Universität für Angewandte Kunst Wien'
@@ -108,6 +112,28 @@ class TestFeature1677(TestCase):
             FakeBidirectionalConceptsMapper.from_entry(entry_with_no_location)
         )
 
+        # Additional full API-User Test:
+        media_with_one_location = model_provider.get_media(entry_with_one_location)
+        media_with_two_locations = model_provider.get_media(entry_with_two_locations)
+        client = ClientProvider(model_provider)
+        for media in (media_with_one_location, media_with_two_locations):
+            response = client.get_media_primary_key_response(media, only_validate=False)
+            if response.status_code != 200:
+                raise RuntimeError(f'Can not perform test. Archival failed {response.status_code} {response.content}')
+        entry_with_one_location.refresh_from_db()
+        entry_with_two_locations.refresh_from_db()
+        phaidra_responses = [
+            requests.get(f'https://services.phaidra-sandbox.univie.ac.at/api/object/{entry.archive_id}/jsonld')
+            for entry in (entry_with_one_location, entry_with_two_locations)
+        ]
+        for response in phaidra_responses:
+            if response.status_code != 200:
+                raise RuntimeError(f'Can not perform test. Phaidra returned {response.status_code} {response.content}')
+        phaidra_data = [response.json() for response in phaidra_responses]
+        cls.phaidra_data_with_1_location, cls.phaidra_data_with_2_locations = phaidra_data
+
+
+
     def test_translated_data(self):
         self.assertIn('bf:physicalLocation', self.translated_data_with_no_location['metadata']['json-ld'])
         self.assertIn('bf:physicalLocation', self.translated_data_with_one_location['metadata']['json-ld'])
@@ -156,4 +182,22 @@ class TestFeature1677(TestCase):
         self.assertIs(
             self.thesis_schema.fields['metadata'].nested.fields['json_ld'].nested.fields['bf_physicalLocation'].nested,
             ValueLanguageBaseSchema,
+        )
+
+    def test_phaidra(self):
+        self.assertIn(
+            'bf:physicalLocation',
+            self.phaidra_data_with_1_location
+        )
+        self.assertIn(
+            'bf:physicalLocation',
+            self.phaidra_data_with_2_locations
+        )
+        self.assertEqual(
+            [self.location_1_phaidra, ],
+            self.phaidra_data_with_1_location['bf:physicalLocation']
+        )
+        self.assertEqual(
+            [self.location_1_phaidra, self.location_2_phaidra ],
+            self.phaidra_data_with_2_locations['bf:physicalLocation']
         )
