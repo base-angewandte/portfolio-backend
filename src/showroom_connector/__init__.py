bff00dde7ca39121dd09c376f29be36b894aa3ea
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2022-02-23 15:51:08 +0100

    + add push_medium func to showroom sync

--- src/showroom_connector/__init__.py
+++ src/showroom_connector/__init__.py
@@ -1,1 +1,7 @@
 default_app_config = 'showroom_connector.apps.ShowroomConnectorConfig'
+
+# TODO: copied from media_server/models.py due to circular import error
+AUDIO_TYPE = 'a'
+DOCUMENT_TYPE = 'd'
+IMAGE_TYPE = 'i'
+VIDEO_TYPE = 'v'
--- src/showroom_connector/sync.py
+++ src/showroom_connector/sync.py
@@ -2,6 +2,8 @@ import requests
 
 from django.conf import settings
 
+from . import AUDIO_TYPE, DOCUMENT_TYPE, IMAGE_TYPE, VIDEO_TYPE
+
 
 class ShowroomError(Exception):
     pass
@@ -61,7 +63,57 @@ def delete_entry(entry):
     elif r.status_code == 400:
         raise ShowroomError(f'Entry {entry.id} could not be pushed: 400: {r.text}')
     elif r.status_code == 204:
-        print(f'deleted {entry.id}')
+        return True
+    else:
+        raise ShowroomError(f'Ouch! Something unexpected happened: {r.status_code} {r.text}')
+
+
+def push_medium(medium):
+    medium_data = medium.get_data()
+    data = {
+        'file': medium.file.url,
+        'type': medium.type,
+        'mime_type': medium.mime_type,
+        'exif': medium.exif,
+        'license': medium.license,
+        'source_repo_media_id': medium.id,
+        'source_repo_entry_id': medium.entry_id,
+        'specifics': {},
+    }
+    if medium.type == IMAGE_TYPE:
+        data['specifics'] = {
+            'previews': medium_data.get('previews'),
+            'thumbnail': medium_data.get('thumbnail'),
+        }
+    elif medium.type == AUDIO_TYPE:
+        data['specifics'] = {
+            'duration': medium_data['metadata'].get('Duration'),
+            'mp3': medium_data.get('mp3'),
+        }
+    elif medium.type == VIDEO_TYPE:
+        data['specifics'] = {
+            'duration': medium_data['metadata'].get('Duration'),
+            'cover': medium_data.get('cover'),
+            'playlist': medium_data.get('playlist'),
+        }
+    elif medium.type == DOCUMENT_TYPE:
+        data['specifics'] = {
+            'cover': medium_data.get('cover'),
+            'pdf': medium_data.get('pdf'),
+            'thumbnail': medium_data.get('thumbnail'),
+        }
+
+    headers = {
+        'X-Api-Client': str(settings.SHOWROOM_REPO_ID),
+        'X-Api-Key': settings.SHOWROOM_API_KEY,
+    }
+    r = requests.post(settings.SHOWROOM_API_BASE + 'media/', json=data, headers=headers)
+
+    if r.status_code == 403:
+        raise ShowroomAuthenticationError(f'Authentication failed: {r.text}')
+    elif r.status_code == 400:
+        raise ShowroomError(f'Medium {medium.id} could not be pushed: 400: {r.text}')
+    elif r.status_code == 201:
         return True
     else:
         raise ShowroomError(f'Ouch! Something unexpected happened: {r.status_code} {r.text}')
