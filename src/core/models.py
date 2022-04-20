9a67180f3ecfe1715f52ff98088fe6eb943f50ad
Philipp Mayer <philipp.mayer@paradoxon.at>
2022-04-20 13:11:33 +0200

    ~ move showroom signals to showroom_connector

--- src/core/models.py
+++ src/core/models.py
@@ -1,18 +1,15 @@
-import django_rq
 from jsonschema import Draft4Validator, FormatChecker, ValidationError as SchemaValidationError, validate
 
-from django.apps import apps
 from django.conf import settings
 from django.contrib.postgres.fields import JSONField
 from django.contrib.postgres.indexes import GinIndex
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models.signals import post_delete, post_save, pre_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.utils.translation import get_language, gettext_lazy as _
 
 from general.models import AbstractBaseModel, ShortUUIDField
-from showroom_connector import sync
 
 from .managers import EntryManager
 from .schemas import ICON_DEFAULT, get_icon, get_jsonschema, get_schema
@@ -172,58 +169,3 @@ class Relation(AbstractBaseModel):
 def relation_pre_save(sender, instance, *args, **kwargs):
     # ensure that there's only one relation between two entries
     instance.to_entry.remove_relation(instance.from_entry)
-
-
-@receiver(post_save, sender=Entry)
-def entry_post_save(sender, instance, created, *args, **kwargs):
-    if settings.SYNC_TO_SHOWROOM:
-        queue = django_rq.get_queue('default')
-        if instance.published:
-            entry_sync = queue.enqueue(sync.push_entry, entry=instance)
-            # TODO: discuss and implement failure handling
-            # now check for all attached media that are also published and push those too
-            # TODO: discuss: it would be more efficient to only do this if the published
-            #       status itself has changed (vs. all the time anything in an already
-            #       published entry was changed), but we would need to write our own
-            #       update function in the serializer, to set the update_fields in kwargs
-            media_model = apps.get_model('media_server', 'Media')
-            published_media = media_model.objects.filter(entry_id=instance.id, published=True)
-            for medium in published_media:
-                queue.enqueue(sync.push_medium, medium=medium, depends_on=entry_sync)
-            # TODO: similar to media also relations would only have to be pushed after
-            #       publishing and not on every save
-            if instance.from_entries.count():
-                queue.enqueue(sync.push_relations, entry=instance, depends_on=entry_sync)
-        # if the instance was just created but not published, we do nothing. but if its
-        # published status (now) is false and it was not just created, we have to delete
-        # it from Showroom
-        elif not created:
-            queue.enqueue(sync.delete_entry, entry=instance)
-            # TODO: discuss and implement failure handling
-
-
-@receiver(post_delete, sender=Entry)
-def entry_post_delete(sender, instance, *args, **kwargs):
-    if settings.SYNC_TO_SHOWROOM:
-        if instance.published:
-            queue = django_rq.get_queue('default')
-            queue.enqueue(sync.delete_entry, entry=instance)
-            # TODO: discuss and implement failure handling
-
-
-@receiver(post_save, sender=Relation)
-def relation_post_save(sender, instance, *args, **kwargs):
-    if settings.SYNC_TO_SHOWROOM:
-        if instance.from_entry.published and instance.to_entry.published:
-            queue = django_rq.get_queue('default')
-            queue.enqueue(sync.push_relations, entry=instance.from_entry)
-            # TODO: discuss and implement failure handling
-
-
-@receiver(post_delete, sender=Relation)
-def relation_post_delete(sender, instance, *args, **kwargs):
-    if settings.SYNC_TO_SHOWROOM:
-        if instance.from_entry.published and instance.to_entry.published:
-            queue = django_rq.get_queue('default')
-            queue.enqueue(sync.push_relations, entry=instance.from_entry)
-            # TODO: discuss and implement failure handling
--- src/showroom_connector/apps.py
+++ src/showroom_connector/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ShowroomConnectorConfig(AppConfig):
     name = 'showroom_connector'
+
+    def ready(self):
+        # import signal handlers
+        from . import signals  # noqa: F401
