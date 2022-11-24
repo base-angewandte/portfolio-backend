a6cf9f92c1328fe8273bb7fde3230af6cf5a76ab
Philipp Mayer <philipp.mayer@paradoxon.at>
2022-11-24 09:54:08 +0100

    feat: make PRIMO_API_URL configurable

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -425,8 +425,8 @@ LANGUAGES_VOCID = 'languages'
 
 EN_LABELS_TITLE_CASE = env.bool('EN_LABELS_TITLE_CASE', default=True)
 
-PRIMO_API_URL = 'https://apigw.obvsg.at/primo/v1/search'
 ANGEWANDTE_API_KEY = env.str('ANGEWANDTE_API_KEY', default='')
+PRIMO_API_URL = env.str('PRIMO_API_URL', default='https://apigw.obvsg.at/primo/v1/search')
 PRIMO_API_KEY = env.str('PRIMO_API_KEY', default='')
 GEONAMES_USER = env.str('GEONAMES_USER', default=None)
 PELIAS_API_KEY = env.str('PELIAS_API_KEY', default=None)
