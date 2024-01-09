d7d1f68f9e001edd9cedf20c11af1487293c81e5
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2024-01-09 14:38:51 +0100

    refactor: rename migrations after merge

--- src/core/migrations/0021_phaidra_validation_service.py
+++ src/core/migrations/0021_phaidra_validation_service.py
@@ -4,29 +4,22 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('media_server', '0022_auto_20221021_1215'),
+        ('core', '0020_auto_20230822_1854'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='media',
+            model_name='entry',
             name='archive_URI',
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
         migrations.AddField(
-            model_name='media',
+            model_name='entry',
             name='archive_id',
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
         migrations.AddField(
-            model_name='media',
-            name='archive_status',
-            field=models.IntegerField(
-                choices=[(0, 'not archived'), (1, 'to be archived'), (2, 'archival in progress'), (3, 'archived'),
-                         (4, 'error'), (5, 'archival update in queue')], default=0),
-        ),
-        migrations.AddField(
-            model_name='media',
+            model_name='entry',
             name='archive_date',
             field=models.DateTimeField(blank=True, null=True),
         ),
--- src/media_server/migrations/0024_phaidra_validation_service.py
+++ src/media_server/migrations/0024_phaidra_validation_service.py
@@ -4,22 +4,29 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0019_auto_20221121_1114'),
+        ('media_server', '0023_auto_20230822_1854'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='entry',
+            model_name='media',
             name='archive_URI',
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
         migrations.AddField(
-            model_name='entry',
+            model_name='media',
             name='archive_id',
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
         migrations.AddField(
-            model_name='entry',
+            model_name='media',
+            name='archive_status',
+            field=models.IntegerField(
+                choices=[(0, 'not archived'), (1, 'to be archived'), (2, 'archival in progress'), (3, 'archived'),
+                         (4, 'error'), (5, 'archival update in queue')], default=0),
+        ),
+        migrations.AddField(
+            model_name='media',
             name='archive_date',
             field=models.DateTimeField(blank=True, null=True),
         ),
